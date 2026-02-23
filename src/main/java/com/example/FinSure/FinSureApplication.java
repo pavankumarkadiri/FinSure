d6ce8f20b400068594e958c22d9b6e700e764580
pavankumarkadiri <pavankumarno143@gmail.com>
2026-02-23 20:46:11 +0530

    Initial commit - Finsure project setup

---
+++ src/main/java/com/example/FinSure/FinSureApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinSure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FinSure/FinSureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinSure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
